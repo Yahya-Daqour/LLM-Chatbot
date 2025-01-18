8deb714a87b1be685ac1f1cc6e329516eab87cae
Yahya-Daqour <yahyadaqour@gmail.com>
2025-01-18 19:00:10 +0300

    Update run for docker

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-export PYTHONPATH=$PYTHONPATH:/LLM-Chatbot/src/backend #Path to backend directory
+#!/bin/bash
+# export PYTHONPATH=$PYTHONPATH:/LLM-Chatbot/src/backend #Path to backend directory
+export PYTHONPATH=$PYTHONPATH:/app/src/backend
 
-cd src/backend/; python main.py
+cd src/backend/;
+python main.py
